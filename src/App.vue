f4c0e316ba0a1a890c4143a6b1cdf4e33f29ec7b
GiPisano <pisanogiacomo9@gmail.com>
2024-04-14 11:58:17 +0200

    add components 'card' for projects

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,30 @@
 <script>
 import axios from 'axios';
+import ProjectCard from './components/ProjectCard.vue'
 
 export default {
   data() {
     return {
-      title: 'Helloooooo',
+      title: 'Boolfolio',
+      projects: [],
     };
   },
+
+  components: {
+    ProjectCard,
+  },
+
+  created() {
+    axios.get('http://127.0.0.1:8000/api/projects').then((response) => {
+      this.projects = response.data.data;
+    })
+  }
 }
 </script>
 
 <template>
-  <h1>{{ title }}</h1>
+  <h1 class="text-center">{{ title }}</h1>
+  <project-card :projects="projects"></project-card>
 </template>
 
 <style lang="scss">
---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,29 @@
+<script>
+
+
+export default {
+    props: {
+        projects: Object,
+    }
+}
+</script>
+
+<template>
+    <div class="container">
+        <div class="row g-3">
+            <div v-for="project in projects" class="col-3">
+                <div class="card h-100">
+                    <img src="#" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">{{ project.title }}</h5>
+                        <p class="card-text">{{ project.id }}</p>
+                        <p class="card-text">{{ project.description }}</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped></style>
