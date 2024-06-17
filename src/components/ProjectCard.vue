7227abdf3bc6973d824c56ede69f52d5fb570c2b
GiPisano <pisanogiacomo9@gmail.com>
2024-06-17 16:49:51 +0200

    add video project

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -1,51 +1,59 @@
 <script>
 export default {
-    props: {
-        project: Object,
-    },
+  props: {
+    project: Object,
+  },
 
-    computed: {
-        abstract() {
-            const description = this.project.description;
-            return description.length > 40 ? description.substr(0, 45) + '...' : description;
-        }
-    }
-}
+  computed: {
+    abstract() {
+      const description = this.project.description;
+      return description.length > 40
+        ? description.substr(0, 45) + "..."
+        : description;
+    },
+  },
+};
 </script>
 
 <template>
-    <div class="col-4">
-        <div class="card h-100">
-            <img :src="project.image ? project.image : 'https://picsum.photos/200/150'" class="card-img-top" alt="...">
-            <div class="card-body">
+  <div class="col-4">
+    <div class="card h-100">
+      <img
+        :src="project.image ? project.image : 'https://picsum.photos/200/150'"
+        class="card-img-top"
+        alt="..."
+      />
+      <div class="card-body">
+        <h5 class="card-title">{{ project.title }}</h5>
+        <p class="card-text">{{ abstract }}</p>
+      </div>
 
-                <h5 class="card-title">{{ project.title }}</h5>
-                <p class="card-text">{{ abstract }}</p>
-
-            </div>
-
-
-            <ul class="list-group list-group-flush">
-
-
-                <li class="list-group-item"><span v-for="technology in project.technologies" class='badge me-2'
-                        :style="'background-color:' + technology.color">{{ technology.label
-                        }}</span></li>
-                <li class="list-group-item">
-                    <span class='badge mb-2' :style="'background-color:' + project.type.color">{{ project.type.label
-                        }}</span>
-                </li>
-                <li class="list-group-item">
-                    <router-link :to="{ name: 'projects.show', params: { id: project.id } }"
-                        class="btn btn-primary">Details</router-link>
-
-                </li>
-            </ul>
-
-
-
-        </div>
+      <ul class="list-group list-group-flush">
+        <li class="list-group-item">
+          <span
+            v-for="technology in project.technologies"
+            class="badge me-2"
+            :style="'background-color:' + technology.color"
+            >{{ technology.label }}</span
+          >
+        </li>
+        <li class="list-group-item">
+          <span
+            class="badge mb-2"
+            :style="'background-color:' + project.type.color"
+            >{{ project.type.label }}</span
+          >
+        </li>
+        <li class="list-group-item">
+          <router-link
+            :to="{ name: 'projects.show', params: { id: project.id } }"
+            class="btn btn-primary"
+            >Details</router-link
+          >
+        </li>
+      </ul>
     </div>
+  </div>
 </template>
 
 <style lang="scss" scoped></style>
--- src/pages/PageDetails.vue
+++ src/pages/PageDetails.vue
@@ -1,47 +1,61 @@
 <script>
-import axios from 'axios';
-import { api } from '../store'
+import axios from "axios";
+import { api } from "../store";
 
 export default {
-    data() {
-        return {
-            project: null,
-        }
-    },
+  data() {
+    return {
+      project: null,
+    };
+  },
 
-    created() {
-        const projectId = this.$route.params.id
-        axios.get(api.baseUrl + `/projects/${projectId}`).then((response) => {
-            this.project = response.data;
-        })
-    }
-}
+  created() {
+    const projectId = this.$route.params.id;
+    axios.get(api.baseUrl + `/projects/${projectId}`).then((response) => {
+      this.project = response.data;
+    });
+  },
+};
 </script>
 
 <template>
-    <div class="container" v-if="project">
-        <h5 class="h1 text-center">{{ project.title }}</h5>
-        <div class="row">
-            <div class="col-6">
-                <img :src="project.image ? project.image : 'https://picsum.photos/500/350'" class="img-details"
-                    alt="...">
-            </div>
-            <div class="col-6">
-                <div>
-                    <span class='badge mb-2' :style="'background-color:' + project.type.color">{{ project.type.label
-                        }}</span>
-                </div>
-                <div>
-                    <span v-for="technology in project.technologies" class='badge me-2'
-                        :style="'background-color:' + technology.color">{{ technology.label
-                        }}</span>
-                </div>
-                <p class="">{{ project.description }}</p>
-            </div>
+  <div class="container" v-if="project">
+    <h5 class="h1 text-center">{{ project.title }}</h5>
+    <div class="row">
+      <!-- <div class="col-6">
+        <img
+          :src="project.image ? project.image : 'https://picsum.photos/500/350'"
+          class="img-details"
+          alt="..."
+        />
+      </div> -->
+
+      <div v-if="project.video" class="card-img-top">
+        <video width="100%" controls>
+          <source :src="project.video" type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+      </div>
+      <div class="col-6">
+        <div>
+          <span
+            class="badge mb-2"
+            :style="'background-color:' + project.type.color"
+            >{{ project.type.label }}</span
+          >
         </div>
-
-
+        <div>
+          <span
+            v-for="technology in project.technologies"
+            class="badge me-2"
+            :style="'background-color:' + technology.color"
+            >{{ technology.label }}</span
+          >
+        </div>
+        <p class="">{{ project.description }}</p>
+      </div>
     </div>
+  </div>
 </template>
 
 <script lang="scss" scoped></script>
